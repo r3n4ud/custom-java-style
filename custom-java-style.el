a39d001187b5f300273c54d581862fb1faf3df7c
Renaud AUBIN <renaud.aubin@nibua-r.org>
2011-12-03 23:01:54 +0100

    Fix arglist-cont-nonempty behavior and add c-toggle-auto-newline

--- custom-java-style.el
+++ custom-java-style.el
@@ -22,72 +22,20 @@
 
 ;; Start with the original java style bare definition + arglist-cont-nonempty
 (defconst custom-java-style
-  `((c-basic-offset . 4)
-   (c-comment-only-line-offset . (0 . 0))
-   ;; the following preserves Javadoc starter lines
-   (c-offsets-alist . ((inline-open . 0)
-                       (topmost-intro-cont    . +)
-                       (statement-block-intro . +)
-                       (knr-argdecl-intro     . 5)
-                       (substatement-open     . +)
-                       (substatement-label    . +)
-                       (label                 . +)
-                       (statement-case-open   . +)
-                       (statement-cont        . +)
-                       (arglist-intro  . c-lineup-arglist-intro-after-paren)
-                       (arglist-close  . c-lineup-arglist)
-                       (access-label   . 0)
-                       (inher-cont     . c-lineup-java-inher)
-                       (func-decl-cont . c-lineup-java-throws)
-                       (arglist-cont-nonempty . ++))))
-  "Custom Java Programming Style")
-
-  ;; `((c-recognize-knr-p . nil) ; K&R style argument declarations are not valid
-  ;;   (c-enable-xemacs-performance-kludge-p . t) ; speed up indentation in XEmacs
-  ;;   (c-basic-offset . 4)
-  ;;   (indent-tabs-mode . nil) ; tabs are evil!
-  ;;   (c-comment-only-line-offset . 0)
-  ;;   (c-hanging-braces-alist . ((defun-open after)
-  ;;                              (defun-close before after)
-  ;;                              (class-open after)
-  ;;                              (class-close before after)
-  ;;                              (namespace-open after)
-  ;;                              (inline-open after)
-  ;;                              (inline-close before after)
-  ;;                              (block-open after)
-  ;;                              (block-close . c-snug-do-while)
-  ;;                              (extern-lang-open after)
-  ;;                              (extern-lang-close after)
-  ;;                              (statement-case-open after)
-  ;;                              (substatement-open after)))
-  ;;   (c-hanging-colons-alist . ((case-label)
-  ;;                              (label after)
-  ;;                              (access-label after)
-  ;;                              (member-init-intro before)
-  ;;                              (inher-intro)))
-  ;;   (c-hanging-semi&comma-criteria
-  ;;    . (c-semi&comma-no-newlines-for-oneline-inliners
-  ;;       c-semi&comma-inside-parenlist
-  ;;       c-semi&comma-no-newlines-before-nonblanks))
-  ;;   (c-indent-comments-syntactically-p . nil)
-  ;;   (comment-column . 40)
-  ;;   (c-cleanup-list . (brace-else-brace
-  ;;                      brace-elseif-brace
-  ;;                      brace-catch-brace
-  ;;                      empty-defun-braces
-  ;;                      defun-close-semi
-  ;;                      list-close-comma
-  ;;                      scope-operator))
-  ;;   (c-offsets-alist . ((arglist-intro custom-java-lineup-expression-plus-4)
-  ;;                       (func-decl-cont . ++)
-  ;;                       (member-init-intro . ++)
-  ;;                       (inher-intro . ++)
-  ;;                       (comment-intro . 0)
-  ;;                       (arglist-close . c-lineup-arglist)
-  ;;                       (topmost-intro . 0)
-  ;;                       (block-open . 0)
-  ;;                       (inline-open . 0)
-  ;;                       (substatement-open . 0)
+  ;; `((c-basic-offset . 4)
+  ;;   (indent-tabs-mode . nil)
+  ;;   (c-comment-only-line-offset . (0 . 0))
+  ;;   ;; the following preserves Javadoc starter lines
+  ;;   (c-offsets-alist . ((inline-open . 0)
+  ;;                       (topmost-intro         . 0)
+  ;;                       (topmost-intro-cont    . 0)
+  ;;                       (statement-block-intro . +)
+  ;;                       (knr-argdecl-intro     . 5)
+  ;;                       (substatement-open     . +)
+  ;;                       (substatement-label    . +)
+  ;;                       (label                 . +)
+  ;;                       (statement-case-open   . +)
+  ;;                       ;; (statement-cont        . +)
   ;;                       (statement-cont
   ;;                        .
   ;;                        (,(when (fboundp 'c-no-indent-after-java-annotations)
@@ -95,21 +43,83 @@
   ;;                         ,(when (fboundp 'c-lineup-assignments)
   ;;                            'c-lineup-assignments)
   ;;                         ++))
-  ;;                       (label . /)
-  ;;                       (case-label . +)
-  ;;                       (statement-case-open . +)
-  ;;                       (statement-case-intro . +) ; case w/o {
-  ;;                       (access-label . /)
-  ;;                       (innamespace . 0)
-  ;;                       (arglist-cont-nonempty . ++)))
-  ;;   (c-block-comment-prefix . "*"))
+  ;;                       (arglist-intro  . c-lineup-arglist-intro-after-paren)
+  ;;                       (arglist-close  . c-lineup-arglist)
+  ;;                       (access-label   . 0)
+  ;;                       (inher-cont     . c-lineup-java-inher)
+  ;;                       (func-decl-cont . c-lineup-java-throws)
+  ;;                       (arglist-cont-nonempty . ++))))
   ;; "Custom Java Programming Style")
+
+  `((c-recognize-knr-p . nil) ; K&R style argument declarations are not valid
+    (c-enable-xemacs-performance-kludge-p . t) ; speed up indentation in XEmacs
+    (c-basic-offset . 4)
+    (indent-tabs-mode . nil) ; tabs are evil!
+    (c-comment-only-line-offset . 0)
+    (c-hanging-braces-alist . ((defun-open after)
+                               (defun-close before after)
+                               (class-open after)
+                               (class-close before after)
+                               (namespace-open after)
+                               (inline-open after)
+                               (inline-close before after)
+                               (block-open after)
+                               (block-close . c-snug-do-while)
+                               (extern-lang-open after)
+                               (extern-lang-close after)
+                               (statement-case-open after)
+                               (substatement-open after)))
+    (c-hanging-colons-alist . ((case-label)
+                               (label after)
+                               (access-label after)
+                               (member-init-intro before)
+                               (inher-intro)))
+    (c-hanging-semi&comma-criteria
+     . (c-semi&comma-no-newlines-for-oneline-inliners
+        c-semi&comma-inside-parenlist
+        c-semi&comma-no-newlines-before-nonblanks))
+    (c-indent-comments-syntactically-p . nil)
+    (comment-column . 40)
+    (c-cleanup-list . (brace-else-brace
+                       brace-elseif-brace
+                       brace-catch-brace
+                       empty-defun-braces
+                       defun-close-semi
+                       list-close-comma
+                       scope-operator))
+    (c-offsets-alist . (;;(arglist-intro custom-java-lineup-expression-plus-4)
+                        (func-decl-cont . ++)
+                        (member-init-intro . ++)
+                        (inher-intro . ++)
+                        (comment-intro . 0)
+                        (arglist-close . c-lineup-arglist)
+                        (topmost-intro . 0)
+                        (block-open . 0)
+                        (inline-open . 0)
+                        (substatement-open . 0)
+                        (statement-cont
+                         .
+                         (,(when (fboundp 'c-no-indent-after-java-annotations)
+                             'c-no-indent-after-java-annotations)
+                          ,(when (fboundp 'c-lineup-assignments)
+                             'c-lineup-assignments)
+                          ++))
+                        (label . /)
+                        (case-label . +)
+                        (statement-case-open . +)
+                        (statement-case-intro . +) ; case w/o {
+                        (access-label . /)
+                        (innamespace . 0)
+                        (arglist-cont-nonempty . ++)))
+    (c-block-comment-prefix . "*"))
+  "Custom Java Programming Style")
 
 (defun custom-set-java-style ()
   "Set the current buffer's java-style to my Custom Programming Style. Meant to be added to `java-mode-hook'."
   (interactive)
   (make-local-variable 'c-tab-always-indent)
   (setq c-tab-always-indent t)
+  (c-toggle-auto-newline 1)
   (c-add-style "custom-java-style" custom-java-style t))
 
 (defun custom-make-newline-indent ()
